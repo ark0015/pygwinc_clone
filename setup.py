1024ccc802d4a21170b12399e56f0664803e6860
Jameson Graef Rollins <jrollins@finestructure.net>
2018-05-31 15:31:03 -0700

    setup.py cleanup
    fix name, version and included files

--- setup.py
+++ setup.py
@@ -1,32 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from __future__ import division, print_function, unicode_literals
 from setuptools import find_packages, setup
 
 
-version = '0.9.5'
+version = '0.1'
 
 
 setup_args = dict(
-    name             = 'GWINC',
+    name             = 'gwinc',
     version          = version,
     url              = 'https://git.ligo.org/gwinc/pygwinc',
     author           = 'LIGO Laboratory',
     author_email     = 'jrollins@ligo.caltech.edu ',
     description      = "Gravitation Wave Interferometer Noise Calculator",
-    license          = 'Copyright 2017 LIGO Laboratory',
-    install_requires = [
-        'numpy',
-        'scipy',
-        'matplotlib',
-    ],
-    packages = find_packages(
-        exclude = ['docs',],
-    ),
-    include_package_data = True,
-    zip_safe = True,
-    keywords = 'Noise, LIGO, Gravitational Wave,',
-    classifiers=[
+    license          = 'Copyright 2018 LIGO Laboratory',
+    keywords         = 'Noise, LIGO, Gravitational Wave,',
+    classifiers = [
         'Topic :: Scientific/Engineering',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
@@ -37,6 +26,19 @@ setup_args = dict(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
+
+    install_requires = [
+        'numpy',
+        'scipy',
+        'matplotlib',
+    ],
+
+    packages = find_packages(
+        exclude = ['docs',],
+    ),
+
+    include_package_data = True,
+    zip_safe = True,
 )
 
 if __name__ == "__main__":
