517277ea179fd0a1ddb90ef69b1ffa649638eea9
Jameson Graef Rollins <jrollins@finestructure.net>
2018-06-01 16:55:25 -0700

    move dhdl function from util module into gwinc module
    free up the "util" namespace for other purposes not related to noise
    calculation

--- gwinc/gwinc.py
+++ gwinc/gwinc.py
@@ -1,15 +1,42 @@
 from __future__ import division
 import copy
 import numpy as np
-from numpy import log10, pi, sqrt
+from numpy import pi, sqrt, sin, exp, abs, log10
+import scipy.constants
 from collections import OrderedDict
 import logging
 
 from .precomp import precompIFO
 from . import suspension
-from . import util
 from . import noise
 from .plot import plot_noise
+
+
+def dhdl(f, armlen):
+    """Strain to length conversion for noise power spetra
+
+    This takes into account the GW wavelength and is only important
+    when this is comparable to the detector arm length.
+
+    From R. Schilling, CQG 14 (1997) 1513-1519, equation 5,
+    with n = 1, nu = 0.05, ignoring overall phase and cos(nu)^2.
+    A small value of nu is used instead of zero to avoid infinities.
+
+    Returns the square of the dh/dL function, and the same divided by
+    the arm length squared.
+
+    """
+    c = scipy.constants.c
+    nu_small = 0.05
+    omega_arm = pi * f * armlen / c
+    omega_arm_f = (1 - sin(nu_small)) * pi * f * armlen / c
+    omega_arm_b = (1 + sin(nu_small)) * pi * f * armlen / c
+    sinc_sqr = 4 / abs(sin(omega_arm_f) * exp(-1j * omega_arm) / omega_arm_f
+                       + sin(omega_arm_b) * exp(1j * omega_arm) / omega_arm_b)**2
+    # keep DC value equal to 1
+    sinc_sqr /= sinc_sqr[0]
+    dhdl_sqr = sinc_sqr / armlen**2
+    return dhdl_sqr, sinc_sqr
 
 
 def noise_calc(ifo, f):
@@ -46,7 +73,7 @@ def noise_calc(ifo, f):
     #
     # used for converting displacement to strain in all noise calculations
 
-    ifo.gwinc.dhdl_sqr, ifo.gwinc.sinc_sqr = util.dhdl(f, ifo.Infrastructure.Length)
+    ifo.gwinc.dhdl_sqr, ifo.gwinc.sinc_sqr = dhdl(f, ifo.Infrastructure.Length)
 
     ##############################
 
