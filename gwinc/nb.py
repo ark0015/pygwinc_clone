e9fbcf70351c5445734015fd737d775928565fa2
Jameson Graef Rollins <jrollins@finestructure.net>
2020-01-31 09:04:35 -0800

    nb: tweak budget str

--- gwinc/nb.py
+++ gwinc/nb.py
@@ -78,7 +78,7 @@ class BudgetItem:
     def __str__(self):
         # FIXME: provide info on internal state (load/update/calc/etc.)
         return '<{} {}>'.format(
-            ' '.join([c.__name__ for c in self.__class__.__bases__]),
+            ', '.join([c.__name__ for c in self.__class__.__bases__]),
             self.name,
         )
 
