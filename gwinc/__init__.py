55d1d9e72abc2b2aa9c02b0271ba67f8bc62cb73
Jameson Graef Rollins <jrollins@finestructure.net>
2020-02-05 21:29:47 -0800

    remove unused code from the gwinc() function

--- gwinc/__init__.py
+++ gwinc/__init__.py
@@ -130,8 +130,6 @@ def gwinc(freq, ifo, source=None, plot=False, PRfixed=True):
         pass
         #warning(['Thermal lensing limits input power to ' num2str(pbs/prfactor, 3) ' W']);
 
-    #TODO decide if all this below this should remain, since it is already inside of __main__
-
     # report astrophysical scores if so desired
     score = None
     if source:
@@ -142,48 +140,20 @@ def gwinc(freq, ifo, source=None, plot=False, PRfixed=True):
     # output graphics
 
     if plot:
-        # Report input parameters
-        if ifo.Optics.Type == 'DualCarrier_new':     #include the case for Dual carrier
-            finesseA = 2*pi/ifo.Optics.ITM.TransmittanceD1
-            finesseB = 2*pi/ifo.Optics.ITM.TransmittanceD2
-            pbsA = ifo.Laser.PBSD1
-            pbsB = ifo.Laser.PBSD2
-            logging.info('Finesse for carrier A:  %7.2f' % finesseA)
-            logging.info('Finesse for carrier B:  %7.2f' % finesseB)
-            logging.info('Power Recycling Factor: %7.2f' % ifo.PRCgain)
-            logging.info('Arm power for carrier A:%7.2f kW' % (finesseA*2/pi*pbsA/2/1000))
-            logging.info('Arm power for carrier B:%7.2f kW' % (finesseB*2/pi*pbsB/2/1000))
-            logging.info('Power on beam splitter for carrier A: %7.2f W' % pbsA)
-            logging.info('Power on beam splitter for carrier B: %7.2f W' % pbsB)
-            logging.info('Laser Power for Carrier A:     %7.2f Watt' % ifo.LP1)
-            logging.info('Laser Power for Carrier B:     %7.2f Watt' % ifo.LP2)
-            logging.info('SRM Detuning for Carrier A:    %7.2f degree' % (ifo.Optics.SRM.TunephaseD1*180/pi))
-            logging.info('SRM Detuning for Carrier B:    %7.2f degree' % (ifo.Optics.SRM.TunephaseD2*180/pi))
-            logging.info('SRM transmission for Carrier A:%9.4f' % ifo.Optics.SRM.TransmittanceD1)
-            logging.info('SRM transmission for Carrier B:%9.4f' % ifo.Optics.SRM.TransmittanceD2)
-            logging.info('ITM transmission for Carrier A:%9.4f' % ifo.Optics.ITM.TransmittanceD1)
-            logging.info('ITM transmission for Carrier B:%9.4f' % ifo.Optics.ITM.TransmittanceD2)
-            logging.info('PRM transmission for both:     %9.4f' % ifo.Optics.PRM.Transmittance)
-        else:
-            logging.info('Laser Power:            %7.2f Watt' % ifo.Laser.Power)
-            logging.info('SRM Detuning:           %7.2f degree' % (ifo.Optics.SRM.Tunephase*180/pi))
-            logging.info('SRM transmission:       %9.4f' % ifo.Optics.SRM.Transmittance)
-            logging.info('ITM transmission:       %9.4f' % ifo.Optics.ITM.Transmittance)
-            logging.info('PRM transmission:       %9.4f' % ifo.Optics.PRM.Transmittance)
-            logging.info('Finesse:                %7.2f' % finesse)
-            logging.info('Power Recycling Gain:   %7.2f' % prfactor)
-            logging.info('Arm Power:              %7.2f kW' % (parm/1000))
-            logging.info('Power on BS:            %7.2f W' % pbs)
+        logging.info('Laser Power:            %7.2f Watt' % ifo.Laser.Power)
+        logging.info('SRM Detuning:           %7.2f degree' % (ifo.Optics.SRM.Tunephase*180/pi))
+        logging.info('SRM transmission:       %9.4f' % ifo.Optics.SRM.Transmittance)
+        logging.info('ITM transmission:       %9.4f' % ifo.Optics.ITM.Transmittance)
+        logging.info('PRM transmission:       %9.4f' % ifo.Optics.PRM.Transmittance)
+        logging.info('Finesse:                %7.2f' % finesse)
+        logging.info('Power Recycling Gain:   %7.2f' % prfactor)
+        logging.info('Arm Power:              %7.2f kW' % (parm/1000))
+        logging.info('Power on BS:            %7.2f W' % pbs)
 
         # coating and substrate thermal load on the ITM
         PowAbsITM = (pbs/2) * \
                     np.hstack([(finesse*2/np.pi) * ifo.Optics.ITM.CoatingAbsorption,
                                (2 * ifo.Materials.MassThickness) * ifo.Optics.ITM.SubstrateAbsorption])
-
-        # Stefan's Mysterious TCS Section ~~~~ `~~ ` ` 324@#%@#$ !
-        M = np.array([[ifo.TCS.s_cc, ifo.TCS.s_cs], [ifo.TCS.s_cs, ifo.TCS.s_ss]])
-        S_uncorr = PowAbsITM.T*M*PowAbsITM
-        TCSeff = 1-sqrt(ifo.TCS.SRCloss/S_uncorr)
 
         logging.info('Thermal load on ITM:    %8.3f W' % sum(PowAbsITM))
         logging.info('Thermal load on BS:     %8.3f W' %
@@ -197,4 +167,5 @@ def gwinc(freq, ifo, source=None, plot=False, PRfixed=True):
             logging.info('Stochastic Omega: %4.1g Universes' % score.Omega)
 
         plot_noise(ifo, traces, **plot_style)
+
     return score, noises, ifo
