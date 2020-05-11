6618ac8e43cc289377826b8a94f996a97a190066
Christopher Wipf <wipf@ligo.mit.edu>
2020-05-11 15:47:36 -0700

    gwinc.test: inherit python interpreter
    Pass sys.executable to the gen_cache.sh script, instead of using a
    hard-coded interpreter path

--- gwinc/test/gen_cache.sh
+++ gwinc/test/gen_cache.sh
@@ -8,6 +8,8 @@ fi
 
 git_hash="$1"
 cache_dir="$2"
+python="$3"
+[ -z "$python" ] && python=python3
 
 mkdir -p $cache_dir
 cache_dir=$(cd $cache_dir && pwd)
@@ -19,8 +21,8 @@ git archive $git_hash | tar -x -C $gwinc_dir
 cd $gwinc_dir
 
 export LOG_LEVEL=INFO
-for ifo in $(python3 -c "import gwinc; print(' '.join(gwinc.IFOS))") ; do
-    python3 -m gwinc --save $cache_dir/${ifo}.h5 $ifo
+for ifo in $($python -c "import gwinc; print(' '.join(gwinc.IFOS))") ; do
+    $python -m gwinc --save $cache_dir/${ifo}.h5 $ifo
 done
 
 echo $git_hash > $cache_dir/git_hash
